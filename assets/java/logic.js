3ebd35281bdcc34c9b64b5f4bcec02cb660e1a05
Ben Mizelle <Ben Mizelle>
2017-03-13 18:32:49 -0400

    pseudo-code and start logic

--- assets/java/logic.js
+++ assets/java/logic.js
@@ -1,28 +1,34 @@
-// Connect to API's (and database if using)
-
-// Get data from API's for when the page initially loads and place them 
-	// in their respective divs using jQuery
-
-// Create on click function for when each drop down is clicked (may switch to submit button)
-// When an option in the dropdown is clicked the results change based on
-	// user selects
-
-	// Examples of results:
-		// Avg house price
-		// Avg rent price
-		// Avg salary for job selected
-		// Current weather
-		// Picture of the city or location
-		// Short description of the location and what it is know for
-
-// The results are listed from 1-10 based on best match to user's selections
-// We only get 10 results
-
-// Within the results are other on click functions that will take the user
-	// to the respective website where the API data is being pulled
-		// Example: when the user clicks the current weather it takes them
-		// to the weather channel page of that location, so that the user can
-		// see more data than we are showing.
+// on page load, display all events from eventful API's
+// As the user makes selections the list of events narrows it reflect the user's taste
+// The results should display the best match listed first
+// limit of ten total results
+// Once the user decides on an event, they can compare the prices of Lyft vs Uber, pricing pulled from respective API's
 
 
 
+
+// Begin by initializring global variables
+var eventCategory = ["sports", "music", "theatre", ]
+var eventLocation = " "
+var eventDate = " "
+
+// identifies the function called displayEvents
+function displayEvents() {
+// initializes variables for data pulled from Eventful API
+      var event = $(this).attr("data-name");
+// API key variable
+      var queryURL = "https://website url here" +
+        event + "&api_key=OUR API KEYlimit=10";
+// Ajax call to display results
+$.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+      
+        .done(function(response) {
+
+        	var results = response.data;
+        	// insert variables or arrays here
+        	// This will let us reload divs and ID's without resetting the rest of the page
+        	// can also set mthod to "push" depending on the situation
+        }
