d8ef4ae6595ad870c4b755f58bc7e3fdf6cda76c
Caromin <c.aromin.unf@gmail.com>
2017-03-14 15:24:11 -0400

    added variables

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,24 @@
 var location = "";
 var category = "";
-var date = "";
 var time = "";
 var price = 0;
+
+var eventList = [Concerts, Nightlife, Outdoors, Conferences, Sports, All ];
+var dateList = [Today, Week, Weekend, Month];
+
+//functions that will generate the list of buttons for some of the variable selections
+function radioButtons {
+	for (i = 0; i < eventList.length; i++) {
+
+	}
+
+	for (i= 0; i < dateList.length; i++) {
+
+	}
+};
+
+//When the page loads, this will run
+$(document).on("load", function() {
+
+radioButtons();
+});
