bda952733a6ef58f239e28dd32767ea11f5ded46
Caromin <c.aromin.unf@gmail.com>
2017-03-14 16:42:46 -0400

    finished radiobuttons w/ids

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,24 +1,31 @@
-var location = "";
+var place = "";
 var category = "";
 var time = "";
-var price = 0;
 
-var eventList = [Concerts, Nightlife, Outdoors, Conferences, Sports, All ];
-var dateList = [Today, Week, Weekend, Month];
+var eventList = ["Concerts ", "Nightlife ", "Outdoors ", "Conferences ", "Sports ", "All Events " ];
+var dateList = ["Today ", "Week ", "Weekend ", "Month "];
 
 //functions that will generate the list of buttons for some of the variable selections
-function radioButtons {
+function radioButtons() {
 	for (i = 0; i < eventList.length; i++) {
-
+		var ids = $('#event'+ i);
+		var button = $('<input type="radio" > ');
+		button.attr("button-value", eventList[i]);
+		button.attr('id', 'eventList' + i);
+		$('#event-input').append(button, eventList[i]);
 	}
 
 	for (i= 0; i < dateList.length; i++) {
-
+		var button = $('<input type="radio" > ');
+		button.attr("button-value", dateList[i]);
+		button.attr('id', 'dateList' + i);
+		$('#date-input').append(button, dateList[i]);
 	}
-};
+}
 
 //When the page loads, this will run
-$(document).on("load", function() {
+$(document).ready(function() {
 
 radioButtons();
+
 });
