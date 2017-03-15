2835203de674517efd34990d4263f6e9a5322962
Brendon McDonald <brendonmcdonald@Brendons-MacBook-Air.local>
2017-03-14 21:11:44 -0400

    trail.api

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -28,4 +28,23 @@ $(document).ready(function() {
 
 radioButtons();
 
+// var queryURL = "https://trailapi-trailapi.p.mashape.com/" 
+
+		$.ajax({
+    url: "https://trailapi-trailapi.p.mashape.com/", // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+    type: 'GET', // The HTTP Method
+    data: {}, // Additional parameters here
+    datatype: 'json',
+    //success: function(data) { alert(JSON.stringify(data)); },
+    error: function(err) { alert(err); },
+    beforeSend: function(xhr) {
+    xhr.setRequestHeader("X-Mashape-Authorization", "NQTdn7V99JmshrgWNZDbdFehWFX8p17WiaijsnBkVdo5einCNy"); // Enter here your Mashape key
+    }
+}).done(function(response){
+console.log(response);
+})
+		
+
+
 });
+
