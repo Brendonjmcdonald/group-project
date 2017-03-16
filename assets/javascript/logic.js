cb3a33d91395d99972de5c4ea1fd17abc421f870
Caromin <c.aromin.unf@gmail.com>
2017-03-15 23:44:57 -0400

    minor fixes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -22,47 +22,45 @@ function checkBox() {
 //this is the function that runs at the end of the googleAPI script 
 function initMap() {
 //where the map will be displayed	
-	mapDiv = $('#input-display');
-//generating the map with the specific parameters	
-	map = new google.maps.Map(mapDiv[0], {
-//set center of map on load
-	center: {lat: lat, lng: lng},
-//set zoom, lower number means zoom out, and vice versa
-	zoom: zoom,
-//you can set map type to roadmap, terrain, satellite, or hybrid
-	mapTypeId: 'hybrid',
-//If you plan to move a control on the screen
-//it is recommened to set that control to true to always show
-	mapTypeControl: true,
-//this sets the position, go to docs to see other positions
-	mapTypeControlOptions: {
-	position: google.maps.ControlPosition.TOP_LEFT
-	},
-//same as before, just different controls
-	fullscreenControl: true,
-	fullscreenControlOptions: {
-	position: google.maps.ControlPosition.LEFT_TOP
-	},
-	streetViewControl: true,
-	streetViewControlOptions: {
-	position: google.maps.ControlPosition.LEFT_TOP 
-	},
-	zoomControl: true,
-	zoomControlOptions: {
-	position: google.maps.ControlPosition.LEFT_TOP
-	}
-});
-
-	// creates a marker
+		mapDiv = $('#input-display');
+	//generating the map with the specific parameters	
+		map = new google.maps.Map(mapDiv[0], {
+	//set center of map on load
+		center: {lat: lat, lng: lng},
+	//set zoom, lower number means zoom out, and vice versa
+		zoom: zoom,
+	//you can set map type to roadmap, terrain, satellite, or hybrid
+		mapTypeId: 'hybrid',
+	//If you plan to move a control on the screen
+	//it is recommened to set that control to true to always show
+		mapTypeControl: true,
+	//this sets the position, go to docs to see other positions
+		mapTypeControlOptions: {
+		position: google.maps.ControlPosition.TOP_LEFT
+		},
+	//same as before, just different controls
+		fullscreenControl: true,
+		fullscreenControlOptions: {
+		position: google.maps.ControlPosition.LEFT_TOP
+		},
+		streetViewControl: true,
+		streetViewControlOptions: {
+		position: google.maps.ControlPosition.LEFT_TOP 
+		},
+		zoomControl: true,
+		zoomControlOptions: {
+		position: google.maps.ControlPosition.LEFT_TOP
+		}
+	});
+// creates a marker
 	var marker = new google.maps.Marker({
 		position: {lat: 28.741898, lng:-81.305587},
 		map: map,
 		clickableIcons: true,
 	});
-
-	// creates the information to be displayed when clicked
+// creates the information to be displayed when clicked
 	var infowindow = new google.maps.InfoWindow({
-		content: '<p>Trail Name ' + 'Activity' + marker.getPosition() + '</p>',
+		content: '<p id="Trail1">Trail Name ' + 'Activity' + marker.getPosition() + '</p>',
 	});
 
 	google.maps.event.addListener(marker, 'click', function(){
@@ -75,10 +73,16 @@ $(document).ready(function() {
 	checkBox();
  	$("#submit-button").on("click", function(event) {
     event.preventDefault();
+
 	});
-
-
-// var queryURL = "https://trailapi-trailapi.p.mashape.com/" 
+    $("#homepage").on("click", function(event) {
+    event.preventDefault();
+	window.location.href = 'index.html';
+	});
+    $("#aboutUs").on("click", function(event) {
+    event.preventDefault();
+	window.location.href = 'aboutus.html'
+	});
 
 		$.ajax({
     url: "https://trailapi-trailapi.p.mashape.com/", // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
@@ -116,29 +120,4 @@ console.log(response.places[i].activities[0]);
 
 
 });
-
-
-
-// var queryURL = "https://trailapi-trailapi.p.mashape.com/" 
-
-// $.ajax({
-//     url: "https://trailapi-trailapi.p.mashape.com/", // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
-//     type: 'GET', // The HTTP Method
-//     data: {}, // Additional parameters here
-//     datatype: 'json',
-//     //success: function(data) { alert(JSON.stringify(data)); },
-//     error: function(err) { alert(err); },
-//     beforeSend: function(xhr) {
-//     xhr.setRequestHeader("X-Mashape-Authorization", "NQTdn7V99JmshrgWNZDbdFehWFX8p17WiaijsnBkVdo5einCNy"); // Enter here your Mashape key
-//     }
-// 	})
-
-
-// .done(function(response){
-// console.log(response);
-// })
-		
-
-// $(document).on("click", )
-
 
