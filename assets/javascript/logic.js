5b0ffe91473a392e494ffec181fc6ef8c66b844d
Caromin <c.aromin.unf@gmail.com>
2017-03-17 21:29:07 -0400

    fixing homepage html, working it and css

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -184,7 +184,8 @@ addInfomarker();
 
 //End of the for loop
 	}
-
+    nameList = [];
+	descriptionList = [];		
 });
 
 //End of trailFinder function
@@ -218,7 +219,7 @@ $(window).on("load", function() {
   			longitude = geometry.lng();
   			
 
-// ----------Below is trail API + Marker info------------  			
+// ----------Below is trail API + Initial Marker info/parameters------------  			
   			trailFinder(latitude, longitude);
 //Set a marker at location
   			var marker = new google.maps.Marker({
@@ -243,10 +244,9 @@ $(window).on("load", function() {
 			}
 //End of the geocode/marker function	  			
 	  	});
-//End of the submit-button function	  
+//End of the submit-button function	 
+//Emptying out the input and emptying out the arrays for the next search 
     $('#name-input').val("");
-    nameList = [];
-	descriptionList = [];	
 	});
 // --------------------------------------------------------
 
@@ -272,7 +272,62 @@ console.log(window.location.href);
 			window.location.href = 'aboutus.html';
 		};
 	});
-
 //End of the window.onload function
-});
+});	
 
+// --------------Kevins Homepage JS---------------------------
+
+
+	var video = $('video')
+	  , container = $('#container');
+
+	var setVideoDimensions = function () {
+	  // Video's intrinsic dimensions
+	  var w = video.videoWidth
+	    , h = video.videoHeight;
+	  
+	  // Intrinsic Ratio
+	  // Will be more than 1 if W > H and less if W < H
+	  var videoRatio = (w / h).toFixed(2);
+	  
+	  // Get the container's computed styles
+	  //
+	  // Also calculate the min dimensions required (this will be
+	  // the container dimentions)
+	  var containerStyles = window.getComputedStyle(container)
+	    , minW = parseInt( containerStyles.getPropertyValue('width') )
+	    , minH = parseInt( containerStyles.getPropertyValue('height') );
+	  
+	  // What's the min:intrinsic dimensions
+	  //
+	  // The idea is to get which of the container dimension
+	  // has a higher value when compared with the equivalents
+	  // of the video. Imagine a 1200x700 container and
+	  // 1000x500 video. Then in order to find the right balance
+	  // and do minimum scaling, we have to find the dimension
+	  // with higher ratio.
+	  //
+	  // Ex: 1200/1000 = 1.2 and 700/500 = 1.4 - So it is best to
+	  // scale 500 to 700 and then calculate what should be the
+	  // right width. If we scale 1000 to 1200 then the height
+	  // will become 600 proportionately.
+	  var widthRatio = minW / w
+	    , heightRatio = minH / h;
+	  
+	  // Whichever ratio is more, the scaling
+	  // has to be done over that dimension
+	  if (widthRatio > heightRatio) {
+	    var newWidth = minW;
+	    var newHeight = Math.ceil( newWidth / videoRatio );
+	  }
+	  else {
+	    var newHeight = minH;
+	    var newWidth = Math.ceil( newHeight * videoRatio );
+	  }
+	  
+	  video.style.width = newWidth + 'px';
+	  video.style.height = newHeight + 'px';
+	};
+
+	video.addEventListener('loadedmetadata', setVideoDimensions, false);
+	window.addEventListener('resize', setVideoDimensions, false);
